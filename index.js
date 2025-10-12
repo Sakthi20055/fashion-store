ae25f747201f98b1fa93640d8883004b1673770c
Sakthi20055 <157904490+Sakthi20055@users.noreply.github.com>
2025-10-12 11:50:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ function closeNavbar()
 {
     sidenav.style.left="-60%"
 
-var sidenav = document.querySelector(".side-navbar")
-
-function showNavbar()
-{
-    sidenav.style.left="0"
-}
-
-function closeNavbar()
-{
-    sidenav.style.left="-60%"
-
 
 }
+
