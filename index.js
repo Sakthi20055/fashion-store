f9edfb91894f974c9ab7af4156710312ee282a10
Sakthi20055 <157904490+Sakthi20055@users.noreply.github.com>
2025-10-12 11:44:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var sidenav = document.querySelector(".side-navbar")
 
 function showNavbar()
@@ -10,7 +10,6 @@ function closeNavbar()
 {
     sidenav.style.left="-60%"
 
-=======
 var sidenav = document.querySelector(".side-navbar")
 
 function showNavbar()
@@ -22,5 +21,5 @@ function closeNavbar()
 {
     sidenav.style.left="-60%"
 
->>>>>>> 1c15066 (Initial commit with contact.js top alert feature)
+
 }
